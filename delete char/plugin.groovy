b1cca67018fda5e8c614b1d4a89f5e603dcb7d50
Ken Fox <ken.fox@atomicobject.com>
2014-08-02 16:05:59 -0400

    delete-horizontal-space: implement more elisp-like

--- delete char/plugin.groovy
+++ delete char/plugin.groovy
@@ -1,9 +1,8 @@
 import com.intellij.openapi.actionSystem.AnActionEvent
-
 import static liveplugin.PluginUtil.*
 
 // The missing ^D to delete character at point was what sent me on
-// my emacs plug-in extension binge. It's a very simple command that
+// my IntelliJ customization binge. It's a very simple command that
 // shows how the IntelliJ API works.
 //
 // The document roughly corresponds to an emacs buffer. The document
@@ -20,9 +19,9 @@ import static liveplugin.PluginUtil.*
 registerAction("delete-char", "ctrl D") { AnActionEvent event ->
   runDocumentWriteAction(event.project) {
     currentEditorIn(event.project).with {
-      def offset = caretModel.offset
-      if (offset < document.textLength) {
-        document.deleteString(offset, offset + 1)
+      def point = caretModel.offset
+      if (point < document.textLength) {
+        document.deleteString(point, point + 1)
       }
     }
   }
--- delete horizontal space/plugin.groovy
+++ delete horizontal space/plugin.groovy
@@ -1,20 +1,36 @@
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.util.TextRange
-
 import static liveplugin.PluginUtil.*
-
 import java.util.regex.*
 
 // With more complicated editing commands, you can either work
 // directly on the IntelliJ document object, or convert to plain Java
-// strings and use built-in Groovy string handling.
+// data and use built-in Groovy string handling.
 //
-// This example converts to Java strings.
+// This example uses Java CharSequence.
+
+def skipCharsForward(buf, point, skipChars) {
+  for (int i = point; i < buf.length(); ++i) {
+    if (0 > skipChars.indexOf((int)buf.charAt(i))) {
+      return i
+    }
+  }
+  return buf.length()
+}
 
 def skipPatternForward(document, pos, pattern) {
   def buf = document.getText(new TextRange(pos, document.textLength))
   Matcher match = buf =~ /^${pattern}+/
   return match.find() ? pos + match.end() : pos
+}
+
+def skipCharsBackward(buf, point, skipChars) {
+  for (int i = point - 1; i >= 0; --i) {
+    if (0 > skipChars.indexOf((int)buf.charAt(i))) {
+      return i + 1
+    }
+  }
+  return 0
 }
 
 def skipPatternBackward(document, pos, pattern) {
@@ -30,8 +46,9 @@ registerAction("delete-horizontal-space", "alt BACK_SLASH") { AnActionEvent even
   runDocumentWriteAction(event.project) {
     currentEditorIn(event.project).with {
       def point = caretModel.offset
-      document.deleteString(skipPatternBackward(document, point, "[ \t]"),
-                            skipPatternForward(document, point, "[ \t]"))
+      def buf = document.charsSequence
+      document.deleteString(skipCharsBackward(buf, point, " \t"),
+                            skipCharsForward(buf, point, " \t"))
     }
   }
 }
